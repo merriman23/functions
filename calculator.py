1cfc34e090e4b1c5e025dd4a1f0b20b2dcff6807
Michael Merriman <michaelmerriman@mail.weber.edu>
2021-04-05 09:13:06 -0600

    optimized menu

--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
 # Get information from the user
-# TODO user input validation
+# TODO user input validation for menu
+# TODO user input validation for number
+# user input on the type of conversion
+# user enters in to convert in to mm, mm for mm to in
+# user enters 1 for in to mm, 2 for mm to in
+userConversionInput = input("What type of conversion \n\t1-inches to mm \n\t2-mm to inches: ")
+
 userInput = input("What is your number: ")
 
 
@@ -8,10 +14,11 @@ print(type(userInput))
 # Perform the conversion
 # Convert in to mm 25.4 mm : 1 in
 # Convert from in to mm  (in * 25.4)
-userAnswer = userNumber * 25.4
-
+if userConversionInput == '1':
+    userAnswer = userNumber * 25.4
 # Convert from mm to in 
-userAnswer = userNumber / 25.4
+if userConversionInput == '2':
+    userAnswer = userNumber / 25.4
 
 # Print out the answer to the user
-print("The answer is: " + userAnswer)
+print("The answer is:", userAnswer)
