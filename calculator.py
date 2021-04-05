58f51bc142b866075506f331d20587f361a304ce
Michael Merriman <michaelmerriman@mail.weber.edu>
2021-04-05 08:54:07 -0600

    mm and in conversions

--- calculator.py
+++ calculator.py
@@ -3,7 +3,15 @@
 userInput = input("What is your number: ")
 
 
-userNumber = int(userInput)
+userNumber = float(userInput)
 print(type(userInput)) 
 # Perform the conversion
-# Print out the anser to the user
+# Convert in to mm 25.4 mm : 1 in
+# Convert from in to mm  (in * 25.4)
+userAnswer = userNumber * 25.4
+
+# Convert from mm to in 
+userAnswer = userNumber / 25.4
+
+# Print out the answer to the user
+print("The answer is: " + userAnswer)
